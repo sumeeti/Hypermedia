883106df253472e021f2e4860092a2154e13d936
Sumeeti <sumeeti.lalwani@commonfloor.com>
2016-03-22 21:22:00 +0530

    first commit

---
+++ test.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sumeeti
+ * Date: 22/3/16
+ * Time: 9:18 PM
+ */
+
+require_once './Client.php';
+$client = new Client("https://api.github.com");
+//var_dump($client->getUri());
+$user = $client->users('captn3m0');
+//var_dump($user);
+$repos = $user->repos();
+//var_dump($repos);
+$repo = $repos[0];
+//var_dump($repo);
+$issues = $repo->issues();
+//var_dump($issues);
